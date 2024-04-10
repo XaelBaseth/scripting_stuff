f52deabaac4816ab3abd1be320a24f5b821a3df0
axel <xaelbaseth@gmail.com>
2024-04-10 14:36:40 +0200

    cleaned up the script!

--- vm_cleaner.sh
+++ vm_cleaner.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-space_before=$(df -h --output=size --total | awk 'END {print $1}')
-
 # Clean APT cache
 sudo apt-get clean
 sudo apt-get autoremove
@@ -16,8 +14,4 @@ docker system prune -a --volumes -f
 # Remove the compressed logs
 sudo find /var/log -type f -name "*.gz" -delete
 
-space_after=$(df -h --output=size --total | awk 'END {print $1}')
-
-space_reclaimed=(echo "$space_before - $space_after\n" | bc)
-
-printf "Cleaning complete :)\nYou have reclaimed $space_reclaimed !\n\n"
+printf "Cleaning complete :)\n\n"
